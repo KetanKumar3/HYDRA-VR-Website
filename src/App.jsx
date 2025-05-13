21da544cab1d972be8c16d68c8d68b4777f891b9
KetanKumar3 <ketanaryan2004@gmail.com>
2025-05-13 21:57:40 +0530

    card section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar"
 import Home from './components/Home'
 import About from "./components/About"
+import Card from "./components/Card"
 
 function App() {
  
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Card />
     </>
   )
 }
